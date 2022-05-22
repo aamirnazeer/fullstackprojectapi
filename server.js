2f9c0b585b5870b7c1326797e144a056d5fb26a1
aamirnazeer <aamirnazeerbhat@gmail.com>
2022-05-22 17:38:23 +0530

    first changes

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3003
+const port = process.env.PORT || 3001
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/api', (req, res) => {
+  res.send('Hello from server!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server started on port: ${port}`)
 })
